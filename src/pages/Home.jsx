573ef2b26835d11988ff180807bafcb2b2dbc7b1
smmiaj <smmiraj2005@.com>
2024-03-28 03:10:44 +0600

    nav bar added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+
+            <div>
+                 <Navbar></Navbar>
+            </div>
+            <h1>hi i am from home</h1>
+        </div>
+    );
+};
+
+export default Home;
