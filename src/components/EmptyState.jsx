36ac694a4baa73fc7f11ae29eff374ecf1f5aa5b
smmiaj <smmiraj2005@.com>
2024-03-30 01:15:48 +0600

    finished

--- src/components/EmptyState.jsx
+++ src/components/EmptyState.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 const EmptyState = ({message, address, label}) => {
     return (
-        <div className="min-h-[calc(100vh-134px) flex gap-5 flex-col items-center justify-center pb-16">
+        <div className="min-h-[calc(100vh-134px)] flex gap-5 flex-col items-center justify-center ">
            <p className="text-3xl text-red-600 font-bold">{message}</p>
            <Link to={address} className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
       <span className="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05] absolute"></span>
@@ -24,4 +24,5 @@ EmptyState.propTypes ={
     label:PropTypes.string.isRequired
 }
 
+console.log();
 export default EmptyState;
